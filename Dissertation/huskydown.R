6086b967e4b206f6db3be4e534aa7613d7647ba8
Megan Feddern <mfeddern@users.noreply.github.com>
2021-05-17 09:30:48 -0700

    Adding HuskyDown

---
+++ Dissertation/huskydown.R
@@ -0,0 +1,4 @@
+if (!require("devtools")) install.packages("devtools", repos = "http://cran.rstudio.org")
+devtools::install_github("rstudio/bookdown")
+devtools::install_github("benmarwick/huskydown")
+
