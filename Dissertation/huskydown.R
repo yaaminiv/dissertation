74be17d375b7151768956adcb1d1790c7985bbf0
Megan Feddern <mfeddern@users.noreply.github.com>
2021-05-22 12:07:12 -0700

    Update huskydown.R

--- Dissertation/huskydown.R
+++ Dissertation/huskydown.R
@@ -1,3 +1,10 @@
+install.packages(c('tinytex', 'rmarkdown'))
+tinytex::install_tinytex()
+# after restarting RStudio, confirm that you have LaTeX with 
+tinytex:::is_tinytex()
+
+
+
 if (!require("devtools")) install.packages("devtools", repos = "http://cran.rstudio.org")
 devtools::install_github("rstudio/bookdown")
 devtools::install_github("benmarwick/huskydown")
